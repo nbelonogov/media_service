0d9ae6770ced9113441405ffc64f8ec7e42e61c0
nbelonogov <nbelonogov96@gmail.com>
2024-11-27 22:17:48 +0700

    added cron

--- media-app/main.py
+++ media-app/main.py
@@ -1,29 +1,38 @@
 import uuid
+
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.interval import IntervalTrigger
 from fastapi import FastAPI, UploadFile, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.database import init_db, get_session
 from app.models import FileMetadata
+from app.tasks import cleanup_files
 from app.utils import save_to_disk, upload_to_cloud, get_file_path
 from app.schemas import FileResponse
 
 app = FastAPI()
 
 
+scheduler = BackgroundScheduler()
+
+
 @app.on_event("startup")
 async def on_startup():
+    scheduler.add_job(
+        func=cleanup_files,
+        name="cron",
+        trigger=IntervalTrigger(hours=1)
+    )
+    scheduler.start()
     await init_db()
 
 
 @app.post("/upload", response_model=FileResponse)
 async def upload_file(file: UploadFile, db: AsyncSession = get_session()):
     try:
-        # Генерация UID
         file_uid = str(uuid.uuid4())
-        # Сохранение на диск
         file_path, file_size = await save_to_disk(file, file_uid)
-        # Сохранение в облако (условное API)
         await upload_to_cloud(file_path, file_uid)
-        # Сохранение метаданных в БД
         metadata = FileMetadata(
             uid=file_uid,
             original_name=file.filename,
@@ -48,3 +57,8 @@ async def download_file(file_uid: str):
         media_type="application/octet-stream",
         filename=f"{file_uid}"
     )
+
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    scheduler.shutdown()
